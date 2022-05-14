e11b8bb560c1dddf942ed4fa3e7fef307acf1063
nealwaga <waganeal@gmail.com>
2022-05-14 23:06:40 +0300

    creation of login route

--- app/auth/views.py
+++ app/auth/views.py
@@ -18,3 +18,19 @@ def register():
         return redirect(url_for('auth.login'))
         title = 'New Account'
     return render_template('auth/register.html', registration_form=form)
+
+@auth.route('/login', methods = ["GET", "POST"])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user, login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+
+        flash('Invalid password or username')
+
+    title = 'iBlog login'
+    return render_template('auth/login.html',login_form = login_form,title=title)
+
+    
