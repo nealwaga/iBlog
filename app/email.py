819f217145fa0fd6a15fe1a2e588dbd4154d44eb
nealwaga <waganeal@gmail.com>
2022-05-15 00:00:20 +0300

    creation of mail_message function

---
+++ app/email.py
@@ -0,0 +1,12 @@
+from flask_mail import Message
+from flask import render_template
+from . import mail
+
+
+def mail_message(subject, template, to, **kwargs):
+    sender_email = ''
+
+    email = Message(subject, sender=sender_email, recipients=[to])
+    email.body = render_template(template + '.txt', **kwargs)
+    email.html = render_template(template + '.html', **kwargs)
+    mail.send(email)
