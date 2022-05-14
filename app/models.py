a98b3f6563640cd52392802e7d6a8c2847c13e73
nealwaga <waganeal@gmail.com>
2022-05-15 02:15:54 +0300

    property class

--- app/models.py
+++ app/models.py
@@ -19,3 +19,22 @@ class User(UserMixin,db.Model):
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
     secure_password = db.Column(db.String(255),nullable = False)
+
+
+    @property
+    def set_password(self):
+        raise AttributeError('You cannot read the password attribute')
+        
+    @set_password.setter
+    def password(self, password):
+        self.secure_password = generate_password_hash(password)
+    def verify_password(self, password):
+        return check_password_hash(self.secure_password,password) 
+    def save_u(self):
+        db.session.add(self)
+        db.session.commit()
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+    def __repr__(self):
+        return f'User {self.username}'
