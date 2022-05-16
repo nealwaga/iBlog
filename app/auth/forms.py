c2620f5094533349071d81b57fe8747d1f5f9ac1
nealwaga <waganeal@gmail.com>
2022-05-16 16:36:35 +0300

    fixed bug

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,17 +1,17 @@
 #from wsgiref.validate import validator
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import InputRequired, Email, EqualTo
+from wtforms.validators import DataRequired, Email, EqualTo
 from ..models import User
 from wtforms import ValidationError
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 
 
 class RegistrationForm(FlaskForm):
-    email= StringField('Your Email Address', validators=[InputRequired(), Email()])
-    username = StringField('Enter your username', validators=[InputRequired()])
-    password = PasswordField('Password', validators =[InputRequired(), EqualTo('password_confirm', message='Passwords must match')])
-    password_confirm = PasswordField('Confirm passwords', validators=[InputRequired])
+    email= StringField('Your Email Address', validators=[DataRequired(), Email()])
+    username = StringField('Enter your username', validators=[DataRequired()])
+    password = PasswordField('Password', validators =[DataRequired(), EqualTo('password_confirm', message='Passwords must match')])
+    password_confirm = PasswordField('Confirm passwords', validators=[DataRequired])
 
     #Validating the email entered
     def validate_email(self, data_field):
@@ -25,7 +25,7 @@ class RegistrationForm(FlaskForm):
 
 
 class LoginForm(FlaskForm):
-    email = StringField('Your Email Address', validators=[InputRequired(), Email()])
-    password = PasswordField('Passsword', validators=[InputRequired()])
+    email = StringField('Your Email Address', validators=[DataRequired(), Email()])
+    password = PasswordField('Passsword', validators=[DataRequired()])
     remember = BooleanField('Remember me')
     submit = SubmitField('Sign in')
